04bd0667e8dfce8c5d820d4838f9a7db08fce4c4
jripke74@gmail.com <jripke74@gmail.com>
2019-12-31 19:28:34 -0800

    add index.hbs file

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to server
+app.use(espress.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index')
+})
